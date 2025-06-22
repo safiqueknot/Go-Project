9d537f53a7d03dbd4e0937ddc3982e77647ae2a7
S Hasan Sohan <safique.dev@gmail.com>
2025-06-22 15:20:11 +0600

    Add files via upload

---
+++ first program/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+    /*
+    int
+    floot
+    string
+    bool 
+    */
+    x := 50 // int
+    x = 100 // reassigning value
+
+    fmt.Println(x)
+
+    y := "masha" // string
+    fmt.Println(y)
+
+    p := true // bool
+    fmt.Println(p)
+}
